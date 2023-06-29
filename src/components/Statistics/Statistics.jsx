8d9e7127489838ba61107eb923b97cdf07723a6b
Iga Ryś <imirys96@gmail.com>
2023-06-30 00:52:29 +0200

    percentage color

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,29 +1,35 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
-
+const colorChange = ( positivePercentage ) =>
+  positivePercentage <= 45
+    ? 'red'
+    : positivePercentage <= 65
+    ? 'orange'
+      : 'green';
+    
 export const Statistics = ({
   good,
   neutral,
   bad,
   total,
   positivePercentage,
-  colorChange,
+
 }) => (
-  (colorChange = positivePercentage <= 45 ? 'red' : (positivePercentage <= 65 ? 'orange' : 'green'))
-  (
+  //  colorChange =  positivePercentage <= 45 ? 'red' : (positivePercentage <= 65 ? 'orange' : 'green') 
+
+ (
     <div>
       <span className={css.stats}>Good: {good}</span>
       <span className={css.stats}>Neutral: {neutral}</span>
       <span className={css.stats}>Bad: {bad}</span>
       <span className={css.total}>Total:{total}</span>
       <p className={css.overall}>
-        Positive feedback: 
-        <span style={{ color: colorChange , marginLeft: '3px'}}>{positivePercentage}%</span>
+        Positive feedback:
+        <span style={{color: colorChange, marginLeft: '3px' }}>{positivePercentage}%</span>
       </p>
     </div>
-  )
-);
+  ));
    
 Statistics.propTypes = {
     good: PropTypes.number,
